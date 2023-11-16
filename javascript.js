c242799dd8032512bb9e83ee5f28c42baaefb828
shinymayo <138251809+shinymayo@users.noreply.github.com>
2023-11-16 08:01:48 +0000

    make dynamic canvas
    created dynamic canvas, saved a previous, global
    loop. need to make the grid one size and resize the
    divs inside to make them more like the game.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,29 @@
 const container = document.querySelector(".canvas");
 // loop to create 16 div'd canvas
-for (i = 0; i < 16; i++) {
+// for (i = 0; i < 16; i++) {
+//     const div = document.createElement("div");
+//     div.classList.add("grid");
+//     container.appendChild(div);
+// }
+
+function createDiv() {
     const div = document.createElement("div");
-    div.classList.add("grid");
-    container.appendChild(div);
+    div.className = "grid";
+    div.innerText = "test";
+
+    return div;
 }
+function createCanvas() {
+    const canvas = document.getElementById("etch-a-sketch");
+    myDivArr = [];
+    // i = 0;
+    // numOfDivs = 16;
+
+    for (i = 0; i < 16; i++) {
+        myDivArr.push(createDiv());
+        canvas.appendChild(myDivArr[i]);
+    }
+    // myDivArr[5].className = "violet"; //this is a test
+    // myDivArr[4].innerText = "hello"; //this is a test
+}
+createCanvas();

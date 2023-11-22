f7f59f425040f1916cf9a966100038a2b602b022
shinymayo <138251809+shinymayo@users.noreply.github.com>
2023-11-22 09:28:11 +0000

    color divs, activate color buttons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+let color = "black";
 // to check that everything on the page is loaded
 window.addEventListener("DOMContentLoaded", () => {
   console.log("DOM loaded");
@@ -20,8 +21,7 @@ function createBoard(size) {
 
   for(i = 0; i < divs; i++) {
     let div = document.createElement("div");
-    div.style.backgroundColor = "green";
-    // div.addEventListener("mouseover", colorDiv);
+    div.addEventListener("mouseover", colorDiv);
     board.insertAdjacentElement("beforeend", div);
   }
 }
@@ -38,57 +38,16 @@ function getSize() {
     return input;
   }
 }
-
+// works with event listener, paints the divs
 function colorDiv() {
-
+  if(color == "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+  } else {
+    this.style.backgroundColor = "black";
+  }
+    
 }
-
-
-// const canvas = document.getElementById("etch-a-sketch");
-// const bigCanvas = document.querySelector(".big-canvas");
-// const smallCanvas = document.querySelector(".small-canvas");
-// const clearBtn = document.querySelector(".clear")
-
-// // create a single div
-// function createDiv() {
-//   const div = document.createElement("div");
-//   div.className = "grid";
-
-//   return div;
-// }
-
-// // create all 16 divs and put them inside the canvas
-// function createCanvas() {
-//   myDivArr = [];
- 
-//   for (i = 0; i < 256; i++) {
-//     myDivArr.push(createDiv());
-//     canvas.appendChild(myDivArr[i]);
-//   }
-// }
-// createCanvas();
-
-// // function to create a canvas with more pixels
-// function createBigCanvas() {
-//     clear();
-//     myDivArr=[];
-//     for (i = 0; i < 1024; i++) {
-//         myDivArr.push(createDiv());
-//         canvas.appendChild(myDivArr[i]);
-//     }
-//     bigCanvas.removeEventListener("click", createBigCanvas);
-// }
-// bigCanvas.addEventListener("click", createBigCanvas);
-
-// // function to activate the hover effect
-// canvas.addEventListener("mouseover", function (e){
-//     if (e.target.matches(".grid")) {
-//         e.target.classList.add("paint");
-//     }
-// })
-
-// // function to clear/reset the grid
-// clearBtn.addEventListener("click", clear);
-// function clear() {
-//     canvas.childNodes.forEach((child) => child.style.backgroundColor = "white");
-// }
+// works with buttons black&random 
+function setColor(colorChoice) {
+  color = colorChoice;
+}
